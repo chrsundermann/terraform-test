35b7efd1082ae7a7c238b0e49e65588d8522b63e
chrsundermann <csundermann@live.de>
2018-02-26 08:27:52 +0100

    changed example.tf

--- example.tf
+++ example.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
-  appid = "418dae67-c1c2-4bc6-ac34-c853e362f7d0"
-  secret_key = "534c1ee6-fec0-414c-ad79-771466b24a62"
-  tenant     = "63f2c9fc-542b-4758-806f-3cc9c6c78fea"
+  client_id     = "418dae67-c1c2-4bc6-ac34-c853e362f7d0"
+  client_secret = "534c1ee6-fec0-414c-ad79-771466b24a62"
+  tenant_id     = "63f2c9fc-542b-4758-806f-3cc9c6c78fea"
 }
 
 # Create a resource group
