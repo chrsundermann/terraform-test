09cc4c8b27f6b9a0016b4df886eeadb4a2833b90
chrsundermann <csundermann@live.de>
2018-03-15 20:42:56 +0100

    some changes

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  alias           = "azurerm.adesso"
-  client_id       = "418dae67-c1c2-4bc6-ac34-c853e362f7d0"
-  client_secret   = "534c1ee6-fec0-414c-ad79-771466b24a62"
-  tenant_id       = "63f2c9fc-542b-4758-806f-3cc9c6c78fea"
-  subscription_id = "c356d2d3-7531-47ce-83bc-8938d820f8b1"
-}
