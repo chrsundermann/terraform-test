b2cc2827189928c08a3d812644b154b6d9ab7ab4
chrsundermann <csundermann@live.de>
2018-03-15 20:39:37 +0100

    commit some changes

--- example.tf
+++ example.tf
@@ -1,8 +1,5 @@
-provider "azurerm" {
-  client_id     = "418dae67-c1c2-4bc6-ac34-c853e362f7d0"
-  client_secret = "534c1ee6-fec0-414c-ad79-771466b24a62"
-  tenant_id     = "63f2c9fc-542b-4758-806f-3cc9c6c78fea"
-  subscription_id = "c356d2d3-7531-47ce-83bc-8938d820f8b1"
+providers {
+  "azurerm" = "azurerm.adesso"
 }
 
 # Create a resource group
@@ -51,10 +48,10 @@ resource "azurerm_subnet" "vpn" {
 }
 
 resource "azurerm_subnet" "dmzin" {
-  name                 = "sn-dmzprivatein"
-  resource_group_name  = "${azurerm_resource_group.rgnet.name}"
-  virtual_network_name = "${azurerm_virtual_network.primhub.name}"
-  address_prefix       = "10.115.0.64/27"
+  name                      = "sn-dmzprivatein"
+  resource_group_name       = "${azurerm_resource_group.rgnet.name}"
+  virtual_network_name      = "${azurerm_virtual_network.primhub.name}"
+  address_prefix            = "10.115.0.64/27"
   network_security_group_id = "${azurerm_network_security_group.privatedmzin.id}"
 }
 
@@ -78,7 +75,6 @@ resource "azurerm_network_security_group" "privatedmzin" {
   name                = "nsg-privatedmzin"
   location            = "${azurerm_resource_group.rgnet.location}"
   resource_group_name = "${azurerm_resource_group.rgnet.name}"
-  
 
   security_rule {
     name                       = "test123"
